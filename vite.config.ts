e0d4a0526a1602b4beea571fc11f1f9277828eeb
Nikhil Saraf <nsaraf@purestorage.com>
2022-04-24 01:13:12 -0700

    fix build mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,29 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import inspect from "vite-plugin-inspect";
 import { DOMElements, SVGElements } from "solid-js/web/dist/dev.cjs";
-export default defineConfig(async () => ({
-  build: {
-    lib: {
-      entry: "./src/index.tsx",
-      formats: ["es", "cjs", "umd"],
-      fileName: "index",
-      name: "SolidThree",
-    },
-    rollupOptions: {
-      external: [
-        "solid-js",
-        "solid-js/web",
-        "solid-js/store",
-        "three",
-        "zustand",
-        "zustand/vanilla",
-      ],
-    },
-    polyfillDynamicImport: false,
-  },
+export default defineConfig(async (mode) => ({
+  build:
+    process.env.BUILD_MODE === "lib"
+      ? {
+          lib: {
+            entry: "./src/index.tsx",
+            formats: ["es", "cjs", "umd"],
+            fileName: "index",
+            name: "SolidThree",
+          },
+          rollupOptions: {
+            external: [
+              "solid-js",
+              "solid-js/web",
+              "solid-js/store",
+              "three",
+              "zustand",
+              "zustand/vanilla",
+            ],
+          },
+          polyfillDynamicImport: false,
+        }
+      : {},
   plugins: [
     // mdx({
     //   transformMDX: (code) => {
