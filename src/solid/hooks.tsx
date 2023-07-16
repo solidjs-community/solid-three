feb8ccf70ca46a59c20f9050116a0c793a16f164
bigmistqke <vandijckv@gmail.com>
2023-07-16 18:47:26 +0200

    (cleanup) remove commented out useFrame

--- src/solid/hooks.tsx
+++ src/solid/hooks.tsx
@@ -51,15 +51,6 @@ export function useThree() {
  * Can order effects with render priority or manually render with a positive priority.
  * @see https://docs.pmnd.rs/react-three-fiber/api/hooks#useframe
  */
-// export function useFrame(callback: RenderCallback, renderPriority: number = 0): null {
-//   const store = useThree()
-//   const subscribe = store.internal.subscribe
-//   // Memoize ref
-//   const ref = useMutableCallback(callback)
-//   // Subscribe on mount, unsubscribe on unmount
-//   useIsomorphicLayoutEffect(() => subscribe(ref, renderPriority, store), [renderPriority, subscribe, store])
-//   return null
-// }
 
 export function useFrame(callback: RenderCallback, renderPriority: number = 0): void {
   const store = useThree()
