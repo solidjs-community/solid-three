195d56734af22254c6ac77becbce9ac0ff7b7958
bigmistqke <vandijckv@gmail.com>
2023-07-16 17:49:00 +0200

    cleanup exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,6 @@
 export * from './events'
 export * from './loop'
-export * from './store'
-export * from './renderer'
-export * from './utils'
 export * from './stages'
-import * as ThreeTypes from '../three-types'
-export { ThreeTypes }
+export * from './store'
+export * from './utils'
+
---
+++ src/solid/index.ts
@@ -0,0 +1,8 @@
+export * from './hooks'
+export * from "./renderer"
+export {
+  context, createRoot
+}
+import { context } from './context'
+import { createRoot } from './renderer'
+
