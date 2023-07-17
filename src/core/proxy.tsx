da8046296e37a231775270b4efccf790016cd816
bigmistqke <vandijckv@gmail.com>
2023-07-17 02:56:33 +0200

    (cleanup) remove unused `const store` and tidy up
    use full names and use similar names as r3f
    remove DEBUG-logs

--- src/core/proxy.tsx
+++ src/core/proxy.tsx
@@ -28,16 +28,13 @@ type ThreeComponentProxy<Source> = {
   [K in keyof Source]: Source[K] extends Constructor ? ThreeComponent<Source[K]> : undefined
 }
 
-let DEBUG = false
 export const makeThreeComponent = <TSource extends Constructor>(source: TSource): ThreeComponent<TSource> => {
-  let Component = (props: any) => {
-    const getParent = useContext(ParentContext)
+  const Component = (props: any) => {
     const store = useThree()
 
     /* Create instance */
     const getInstance = createMemo(() => {
       try {
-        DEBUG && console.log('three', 'createInstance', source, props.args, getParent)
         const el = prepare(new source(...(props.args ?? []))) as Instance
         el.__r3f.root = store
         return el
@@ -57,75 +54,60 @@ export const makeThreeComponent = <TSource extends Constructor>(source: TSource)
 
 export function useInstance(getInstance: () => Instance, props: any) {
   const getParent = useContext(ParentContext)
-  const store = useThree()
   const [local, instanceProps] = splitProps(props, ['ref', 'args', 'object', 'attach', 'children'])
-  createRenderEffect(() => {
-    /* Assign ref */
-    if (props.ref instanceof Function) local.ref(getInstance())
-  })
+
+  /* Assign ref */
+  createRenderEffect(() => props.ref instanceof Function && local.ref(getInstance()))
 
   /* Apply the props to THREE-instance */
   createRenderEffect(() => applyProps(getInstance(), instanceProps))
 
   /* Connect to parent */
   createRenderEffect(() => {
-    let ins = getInstance()
-    let par = getParent!()
-    if (ins instanceof THREE.Object3D && par instanceof THREE.Object3D) {
-      DEBUG && console.log('three', 'insertNode', ins, par)
-      par.add(ins)
-      onCleanup(() => {
-        DEBUG && console.log('three', 'removeNode', ins, par)
-        par.remove(ins)
-      })
+    const child = getInstance()
+    const parent = getParent!()
+    if (child instanceof THREE.Object3D && parent instanceof THREE.Object3D) {
+      parent.add(child)
+      onCleanup(() => parent.remove(child))
     }
-    ins.__r3f.parent = par
-    if (!par.__r3f.objects.includes(ins)) par.__r3f.objects.push(ins)
+    child.__r3f.parent = parent
+    if (!parent.__r3f.objects.includes(child)) parent.__r3f.objects.push(child)
 
     onCleanup(() => {
-      const index = par.__r3f.objects.indexOf(ins)
+      const index = parent.__r3f.objects.indexOf(child)
       if (index > -1) {
-        par.__r3f.objects.splice(index, 1)
+        parent.__r3f.objects.splice(index, 1)
       }
     })
   })
 
   /* Attach */
   createRenderEffect(() => {
+    const child = getInstance()
     let attach: string | undefined = local.attach
-    let ins = getInstance()
     if (!attach) {
-      if (ins instanceof THREE.Material) attach = 'material'
-      else if (ins instanceof THREE.BufferGeometry) attach = 'geometry'
-      else if (ins instanceof THREE.Fog) attach = 'fog'
+      if (child instanceof THREE.Material) attach = 'material'
+      else if (child instanceof THREE.BufferGeometry) attach = 'geometry'
+      else if (child instanceof THREE.Fog) attach = 'fog'
     }
 
-    let parentNode = getParent!()
+    const parent = getParent!()
 
     /* If the instance has an "attach" property, attach it to the parent */
     if (attach) {
-      if (attach in parentNode) {
-        DEBUG && console.log('three', 'attach', attach, ins, parentNode)
-        parentNode[attach] = ins
-        onCleanup(() => void (parentNode[attach!] = undefined))
+      if (attach in parent) {
+        parent[attach] = child
+        onCleanup(() => void (parent[attach!] = undefined))
       } else {
-        console.error(`Property "${attach}" does not exist on parent "${parentNode.constructor.name}"`)
+        console.error(`Property "${attach}" does not exist on parent "${parent.constructor.name}"`)
       }
     }
   })
 
   /* Automatically dispose */
-  if ('dispose' in getInstance())
-    onCleanup(() => {
-      DEBUG && console.log('three', 'dispose', getInstance())
-      getInstance().dispose?.()
-    })
+  if ('dispose' in getInstance()) onCleanup(() => getInstance().dispose?.())
 
-  createEffect(() => {
-    if (props.helper) {
-      useHelper(getInstance, props.helper)
-    }
-  })
+  createEffect(() => props.helper && useHelper(getInstance, props.helper))
 }
 
 export function Primitive<T extends Instance>(props: { object: T; children?: JSXElement }) {
@@ -133,7 +115,7 @@ export function Primitive<T extends Instance>(props: { object: T; children?: JSX
 
   /* Prepare instance */
   const instance = createMemo(() => {
-    let obj = prepare(props.object)
+    const obj = prepare(props.object)
     obj.__r3f.root = store
     return obj
   })
@@ -143,6 +125,9 @@ export function Primitive<T extends Instance>(props: { object: T; children?: JSX
   return <ParentContext.Provider value={instance}>{props.children}</ParentContext.Provider>
 }
 
+/**
+ * Convenience method for setting (potentially nested) properties on an object.
+ */
 export const applyProp = (object: Instance, props: { [key: string]: any }, key: string) => {
   const rootState = getRootState(object as any as THREE.Object3D)
   /* If the key contains a hyphen, we're setting a sub property. */
@@ -178,16 +163,17 @@ export const applyProp = (object: Instance, props: { [key: string]: any }, key: 
   }
 
   if (!rootState) return
+
+  /* If prop is an event-handler */
   if (/^on(Pointer|Click|DoubleClick|ContextMenu|Wheel)/.test(key)) {
     object.__r3f.handlers[key] = props[key]
     object.__r3f.eventCount = Object.keys(object.__r3f.handlers).length
 
     if (rootState.internal && object.raycast) {
-      const index = rootState.internal.interaction.indexOf(object as unknown as THREE.Object3D)
+      const index = untrack(() => rootState.internal.interaction.indexOf(object as unknown as THREE.Object3D))
       if (object.__r3f.eventCount && index === -1) {
-        untrack(() => rootState.set('internal', 'interaction', (arr) => [...arr, object as unknown as THREE.Object3D]))
-      }
-      if (!object.__r3f.eventCount && index !== -1) {
+        rootState.set('internal', 'interaction', (arr) => [...arr, object as unknown as THREE.Object3D])
+      } else if (!object.__r3f.eventCount && index !== -1) {
         rootState.set(
           'internal',
           'interaction',
@@ -198,9 +184,6 @@ export const applyProp = (object: Instance, props: { [key: string]: any }, key: 
   }
 }
 
-/**
- * Convenience method for setting (potentially nested) properties on an object.
- */
 export const applyProps = (object: Instance, props: { [key: string]: any }) =>
   createRenderEffect(
     mapArray(
