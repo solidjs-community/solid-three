2d7293a65ef298ed8ed4df2d221de3eb2d8835da
bigmistqke <vandijckv@gmail.com>
2023-07-18 12:37:38 +0200

    update example

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,20 +1,21 @@
-import { T, Canvas } from "../src";
-import { Box } from "./Box";
+import { T, Canvas } from '../src'
+import { Box } from './Box'
 
 export function App() {
   return (
     <Canvas
       camera={{
-        position: [3, 3, 3]
+        position: [3, 3, 3],
       }}
       gl={{
-        antialias: true
+        antialias: true,
       }}
-      shadows
-    >
-      <Box />
+      shadows>
+      <Box>
+        <Box position={[1, 0, 0]} />
+      </Box>
       <T.AmbientLight />
       <T.SpotLight position={[0, 5, 10]} intensity={1} />
     </Canvas>
-  );
+  )
 }
--- playground/Box.tsx
+++ playground/Box.tsx
@@ -1,23 +1,24 @@
+import { JSX, createEffect, createSignal } from 'solid-js'
+import * as THREE from 'three'
+import { Mesh } from 'three'
+import { T, useFrame } from '../src'
 
-import { createSignal } from "solid-js";
-import * as THREE from "three";
-import { Mesh } from "three";
-import { T, useFrame } from "../src";
-
-export function Box() {
-  let mesh: Mesh | undefined;
-  const [hovered, setHovered] = createSignal(false);
-  
-  useFrame(() => (mesh!.rotation.y += 0.01));
+export function Box(props: { position?: [number, number, number]; children?: JSX.Element }) {
+  let mesh: Mesh | undefined
+  const [hovered, setHovered] = createSignal(false)
+  useFrame(() => {
+    mesh!.rotation.y += 0.01
+  })
 
   return (
     <T.Mesh
+      position={props.position ?? [0, 0, 0]}
       ref={mesh}
-      onPointerEnter={e => setHovered(true)}
-      onPointerLeave={e => setHovered(false)}
-    >
+      onPointerEnter={(e) => setHovered(true)}
+      onPointerLeave={(e) => setHovered(false)}>
+      {props.children}
       <T.BoxGeometry />
-      <T.MeshStandardMaterial color={new THREE.Color(hovered() ? "blue" : "green")} />
+      <T.MeshStandardMaterial color={hovered() ? 'blue' : 'green'} />
     </T.Mesh>
-  );
+  )
 }
