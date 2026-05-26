656baa5eb131d6cfb4cdfff18d8ab20f012c24b3
bigmistqke <vandijckv@gmail.com>
2026-05-26 20:08:20 +0200

    Revert "feat(tutorial): sticky Reset button inside the editor's scroll container"
    This reverts commit de72e82f4aed2dfecd25ad5d62223da025696314.

--- site/src/components/demo.tsx
+++ site/src/components/demo.tsx
@@ -348,13 +348,6 @@ function DemoClient(props: DemoProps) {
       <div class="demo-panes">
         <Show when={!isNarrow() || pane() === "editor"}>
           <div class="demo-editor-wrapper">
-            <Show when={code() !== initialCode}>
-              <div class="demo-reset-sticky">
-                <button type="button" class="demo-reset" onClick={resetCode}>
-                  Reset
-                </button>
-              </div>
-            </Show>
             <TmTextarea
               class="demo-editor"
               grammar="tsx"
@@ -363,6 +356,11 @@ function DemoClient(props: DemoProps) {
               editable
               onInput={event => setCode(event.currentTarget.value)}
             />
+            <Show when={code() !== initialCode}>
+              <button type="button" class="demo-reset" onClick={resetCode}>
+                Reset
+              </button>
+            </Show>
           </div>
         </Show>
         <Show when={!isNarrow() || pane() === "canvas"}>
