77bbbcc8e38385f94536fed9564d8758550b1072
bigmistqke <vandijckv@gmail.com>
2026-05-26 12:07:59 +0200

    feat(tutorial): add chapter loader sorted by part and order

--- tutorial/App.tsx
+++ tutorial/App.tsx
@@ -1,10 +1,19 @@
-import Hello, { frontmatter } from "./chapters/00-hello.mdx"
+import { For } from "solid-js"
+import { chapters } from "./chapter-loader"
 
 export function App() {
   return (
     <main>
-      <p>Frontmatter: {JSON.stringify(frontmatter)}</p>
-      <Hello />
+      <For each={chapters}>
+        {chapter => {
+          const Chapter = chapter.default
+          return (
+            <section id={chapter.frontmatter.id}>
+              <Chapter />
+            </section>
+          )
+        }}
+      </For>
     </main>
   )
 }
